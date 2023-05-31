19a94bc1bf6e693bfc4f517a6876fc72ea13c6d6
zhifusong <1253903640@qq.com>
2023-05-31 14:32:11 +0800

    更新了获取日期接口的位置

---
+++ apis/user/version.ts
@@ -0,0 +1,8 @@
+import {request} from "@/framework/network/request";
+import {buildGetApiByType} from "@/framework/apis"
+import {apiType} from "@/apis";
+
+const buildGetApi = (url: string) => buildGetApiByType(url, apiType.version)
+
+export const getRangeByWeek = (version: string) => request(buildGetApi("/convert"), {version},{},false,false) as Promise<any>
+export const getWeekByDate = (version: string) => request(buildGetApi("/date/convert"), {version},{},false,false) as Promise<any>
