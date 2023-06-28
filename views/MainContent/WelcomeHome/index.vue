c663d633d3e416c9740e4bbe5438c275b7551887
zhifusong <1253903640@qq.com>
2023-06-28 09:08:29 +0800

    将原本在首页中的tiny-mce实例的使用方法移动到了tiny-mce/use.vue中

--- views/MainContent/WelcomeHome/index.vue
+++ views/MainContent/WelcomeHome/index.vue
@@ -1,30 +1,9 @@
 <template>
-  首页
-  <welcome-home @get-content="getContent" />
-  <a-button type="primary" @click="submitInfo">提交信息</a-button>
-  <a-button type="primary" @click="getInfo">获取信息</a-button>
-  <div v-html="content" style="height: 200px;width: 200px;background-color: pink"></div>
+  <h1>首页功能尚未开发，请使用本系统中的其他模块！</h1>
 </template>
 
 <script lang="ts" setup>
-import WelcomeHome from '@/framework/components/common/tiny-mce/index.vue'
-import {Ref} from "vue";
-import axios from "axios";
-let content = ref('')
-let htmlContent:Ref<string> = ref('')
-const submitInfo = () => {
-  console.log(htmlContent.value);
-  axios.post('http://10.3.18.44:8080/om-weekly-report/web/test', {id: htmlContent.value}).then(res => console.log(res))
-}
 
-const getInfo = () => {
-  axios.get('http://10.3.18.44:8080/om-weekly-report/web/test').then(res => {
-    content.value = res.data.payload
-  })
-}
-const getContent = (html: string) => {
-  htmlContent.value = html
-}
 
 </script>
 
