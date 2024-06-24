5653c97c41bb95f28fe70d086521c8887f1f524b
swansrp <56093273@qq.com>
2024-06-24 16:32:46 +0800

    优化登录失败流程

--- network/login.ts
+++ network/login.ts
@@ -1,111 +1,140 @@
-import {getToken, getUserInfo, ssoLogin, verifyLogin} from '@/framework/apis/login/login'
-import {isNotEmpty, localStorageMethods} from '@/framework/utils/common'
-import {AUTHORIZATION_TOKEN, ID_TOKEN, REFRESH_TOKEN} from '@/framework/utils/constant'
+import { getToken, getUserInfo, ssoLogin, verifyLogin } from '@/framework/apis/login/login'
+import { isNotEmpty, localStorageMethods } from '@/framework/utils/common'
+import { AUTHORIZATION_TOKEN, ID_TOKEN, REFRESH_TOKEN } from '@/framework/utils/constant'
 import qs from 'qs'
-import {useRouteStore} from '@/framework/store/route'
+import { useRouteStore } from '@/framework/store/route'
 import pinia from '@/framework/store'
-import {useUserStore} from '@/framework/store/user'
-import {useCommonStore} from '@/framework/store/common'
-import {getQueryObject, removeURLParameter} from '@/framework/network/utils'
+import { useUserStore } from '@/framework/store/user'
+import { useCommonStore } from '@/framework/store/common'
+import { getQueryObject, removeURLParameter } from '@/framework/network/utils'
 import router from '@/framework/router'
-import {LocationQueryRaw} from 'vue-router'
+import { LocationQueryRaw } from 'vue-router'
 
 const userStore = useUserStore(pinia)
 const commonStore = useCommonStore(pinia)
 let afterLoginHandler: Function
+localStorageMethods.setLocalStorage('loginRetryTime', '3')
+const ssoLoginUrl = import.meta.env.VITE_ssoLoginUrl
+export const navigation2Login = () => {
+  let redirectUri
+  if (isNotEmpty(import.meta.env.VITE_ssoDomain)) {
+    redirectUri = import.meta.env.VITE_ssoDomain
+    if (redirectUri === 'localhost') {
+      const url = removeURLParameter(window.location.href, 'redirect_uri').split('#/')[1]
+      const redirect_uri = url === 'login' ? undefined : url
+      return router.replace({
+        path: ssoLoginUrl,
+        query: {redirect_uri} as LocationQueryRaw
+      }).then(() => window.location.reload())
+    }
+  } else {
+    const url = window.location.href.split('://')[1]
+    const http = window.location.href.split(url)[0]
+    redirectUri = http + url.split('?')[0].split('/')[0] + '/sso'
+  }
+  const targetUrl = encodeURIComponent(window.location.href)
+  window.location.href = ssoLoginUrl + '&redirect_uri=' + redirectUri + '&target_url=' + targetUrl
+}
+
 
 const _executeLogin = (token: any) => {
-    if (import.meta.env.VITE_ssoDomain === 'localhost') {
-        const ssoLoginUrl = import.meta.env.VITE_ssoLoginUrl
-        localStorageMethods.setLocalStorage(AUTHORIZATION_TOKEN, token)
-        const url = removeURLParameter(window.location.href, 'redirect_uri').split('#/')[1]
-        return router.replace({path: ssoLoginUrl, query: {redirect_uri: url} as LocationQueryRaw})
-    } else {
-        return ssoLogin(userStore.getIdToken)
-            .then(res => {
-                const payload = res.payload
-                if (payload && payload.accessToken) {
-                    userStore.name = payload.name
-                    localStorageMethods.setLocalStorage(AUTHORIZATION_TOKEN, payload.accessToken)
-                    localStorageMethods.setLocalStorage(REFRESH_TOKEN, payload.refreshToken)
-                    commonStore.hasLogin = true
-                }
-                return 1
-            }, err => {
-                if (err && err.payload && err.payload.ssoLoginUrl) {
-                    console.log(err.payload.ssoLoginUrl)
-                } else {
-                    console.log('token验证失败')
-                    throw new Error(err)
-                }
-            }).catch(err => {
-                console.log('服务器状态异常')
-                throw new Error(err)
-            })
-    }
+  if (import.meta.env.VITE_ssoDomain === 'localhost') {
+    const ssoLoginUrl = import.meta.env.VITE_ssoLoginUrl
+    localStorageMethods.setLocalStorage(AUTHORIZATION_TOKEN, token)
+    const url = removeURLParameter(window.location.href, 'redirect_uri').split('#/')[1]
+    return router.replace({path: ssoLoginUrl, query: {redirect_uri: url} as LocationQueryRaw})
+  } else {
+    return ssoLogin(userStore.getIdToken)
+      .then(res => {
+        const payload = res.payload
+        if (payload && payload.accessToken) {
+          userStore.name = payload.name
+          localStorageMethods.setLocalStorage(AUTHORIZATION_TOKEN, payload.accessToken)
+          localStorageMethods.setLocalStorage(REFRESH_TOKEN, payload.refreshToken)
+          commonStore.hasLogin = true
+          localStorageMethods.setLocalStorage('loginRetryTime', '3')
+        }
+        return 1
+      }, err => {
+        if (err && err.payload && err.payload.ssoLoginUrl) {
+          console.log(err.payload.ssoLoginUrl)
+          const retryTimes = localStorageMethods.getLocalStorage('loginRetryTime')
+          if (Number(retryTimes) - 1 >= 0) {
+            localStorageMethods.setLocalStorage('loginRetryTime', '' + (Number(retryTimes) - 1))
+            navigation2Login()?.then()
+          }
+        } else {
+          console.log('token验证失败')
+          throw new Error(err)
+        }
+      }).catch(err => {
+        console.log('服务器状态异常')
+        throw new Error(err)
+      })
+  }
 }
 
 export const setAfterLoginHandler = (loginHandler: Function) => {
-    afterLoginHandler = loginHandler
+  afterLoginHandler = loginHandler
 }
 export const checkLoginState = async () => {
-    const queryObject = getQueryObject(window.location.href)
-    if (isNotEmpty(queryObject)) {
-        if (isNotEmpty(queryObject.target_url)) {
-            const targetUrl = queryObject.target_url
-            delete queryObject.target_url
-            window.location.href = targetUrl + '?' + qs.stringify(queryObject, {arrayFormat: 'repeat'})
-        } else if (isNotEmpty(queryObject.id_token)) {
-            userStore.setIdToken(<string>queryObject.id_token).then(() => {
-                window.location.href = removeURLParameter(window.location.href, ID_TOKEN)
-            })
-        }
+  const queryObject = getQueryObject(window.location.href)
+  if (isNotEmpty(queryObject)) {
+    if (isNotEmpty(queryObject.target_url)) {
+      const targetUrl = queryObject.target_url
+      delete queryObject.target_url
+      window.location.href = targetUrl + '?' + qs.stringify(queryObject, {arrayFormat: 'repeat'})
+    } else if (isNotEmpty(queryObject.id_token)) {
+      userStore.setIdToken(<string>queryObject.id_token).then(() => {
+        window.location.href = removeURLParameter(window.location.href, ID_TOKEN)
+      })
     }
+  }
 
-    return await getToken().then((getTokenRes) => {
-        const token = getTokenRes.payload.token
-        const localStorageToken = localStorageMethods.getLocalStorage(AUTHORIZATION_TOKEN)
-        if (localStorageToken) {
-            return verifyLogin(localStorageToken).then((res) => {
-                const result = res.payload
-                if (!+result) {
-                    if (window.location.hash === '#/login') {
-                        return localStorageMethods.setLocalStorage(AUTHORIZATION_TOKEN, token)
-                    } else {
-                        return _executeLogin(token).then(() => {
-                            if (window.location.hash !== '#/login') {
-                                return afterLogin()
-                            }
-                        })
-                    }
-                } else {
-                    commonStore.hasLogin = true
-                    return afterLogin()
-                }
+  return await getToken().then((getTokenRes) => {
+    const token = getTokenRes.payload.token
+    const localStorageToken = localStorageMethods.getLocalStorage(AUTHORIZATION_TOKEN)
+    if (localStorageToken) {
+      return verifyLogin(localStorageToken).then((res) => {
+        const result = res.payload
+        if (!+result) {
+          if (window.location.hash === '#/login') {
+            return localStorageMethods.setLocalStorage(AUTHORIZATION_TOKEN, token)
+          } else {
+            return _executeLogin(token).then(() => {
+              if (window.location.hash !== '#/login') {
+                return afterLogin()
+              }
             })
+          }
         } else {
-            if (window.location.hash === '#/login') {
-                return localStorageMethods.setLocalStorage(AUTHORIZATION_TOKEN, token)
-            } else {
-                return _executeLogin(token).then(() => {
-                    if (window.location.hash !== '#/login') {
-                        return afterLogin()
-                    }
-                })
-            }
+          commonStore.hasLogin = true
+          return afterLogin()
         }
-    })
+      })
+    } else {
+      if (window.location.hash === '#/login') {
+        return localStorageMethods.setLocalStorage(AUTHORIZATION_TOKEN, token)
+      } else {
+        return _executeLogin(token).then(() => {
+          if (window.location.hash !== '#/login') {
+            return afterLogin()
+          }
+        })
+      }
+    }
+  })
 }
 
 export const afterLogin = async () => {
-    const routeStore = useRouteStore(pinia)
-    return await routeStore.getDynamicRouteAction().then(getUserInfo).then((res) => {
-        const data = res.payload
-        const userStore = useUserStore(pinia)
-        Object.keys(data).forEach((key: string) => {
-            if (data[key]) { // @ts-ignore
-                userStore[key] = data[key]
-            }
-        })
-    }).then(() => afterLoginHandler && afterLoginHandler())
+  const routeStore = useRouteStore(pinia)
+  return await routeStore.getDynamicRouteAction().then(getUserInfo).then((res) => {
+    const data = res.payload
+    const userStore = useUserStore(pinia)
+    Object.keys(data).forEach((key: string) => {
+      if (data[key]) { // @ts-ignore
+        userStore[key] = data[key]
+      }
+    })
+  }).then(() => afterLoginHandler && afterLoginHandler())
 }
--- network/request.ts
+++ network/request.ts
@@ -1,18 +1,16 @@
 import qs from 'qs'
-import axios, {AxiosResponse} from 'axios'
-import {ElLoading, ElMessage} from 'element-plus'
+import axios, { AxiosResponse } from 'axios'
+import { ElLoading, ElMessage } from 'element-plus'
 import 'element-plus/es/components/loading/style/css'
 import 'element-plus/es/components/message/style/css'
-import {isNotEmpty, localStorageMethods} from '@/framework/utils/common'
-import {LoadingInstance} from 'element-plus/lib/components/loading/src/loading'
-import {AUTHORIZATION_TOKEN} from '@/framework/utils/constant'
-import {useCommonStore} from '@/framework/store/common'
+import { localStorageMethods } from '@/framework/utils/common'
+import { LoadingInstance } from 'element-plus/lib/components/loading/src/loading'
+import { AUTHORIZATION_TOKEN } from '@/framework/utils/constant'
+import { useCommonStore } from '@/framework/store/common'
 import pinia from '@/framework/store'
-import router from '@/framework/router'
-import {LocationQueryRaw} from 'vue-router'
-import {removeURLParameter} from '@/framework/network/utils'
-import {AxiosProgressEvent} from 'axios/index'
-import {baseDomain} from '@/framework/apis'
+import { AxiosProgressEvent } from 'axios/index'
+import { baseDomain } from '@/framework/apis'
+import { navigation2Login } from '@/framework/network/login'
 
 // 全局的 ElLoading，即使多次创建，也只会存在一个，方便随时关闭
 let loadingInstance: LoadingInstance
@@ -22,77 +20,58 @@ const web = '/web'
 const baseURL = import.meta.env.VITE_baseURL + baseDomain + web
 
 const errCode = {
-    SUCCESS: 0,
-    SESSION_TIME_OUT: 401
+  SUCCESS: 0,
+  SESSION_TIME_OUT: 401
 }
 const axiosInstance = axios.create({})
 const createLoadingInstance = () => ElLoading.service({
-    fullscreen: true,
-    text: '加载中，请稍后……',
-    background: 'rgba(0, 0, 0, 0.7)'
+  fullscreen: true,
+  text: '加载中，请稍后……',
+  background: 'rgba(0, 0, 0, 0.7)'
 })
 
 axiosInstance.interceptors.request.use(
-    (config) => {
-        const {data, showLoading} = config.data as configDataType
-        if (showLoading) loadingInstance = createLoadingInstance()
-        config.data = data
-        const token = localStorageMethods.getLocalStorage(AUTHORIZATION_TOKEN)
-        if (token) config.headers['Authorization'] = 'Bearer ' + localStorageMethods.getLocalStorage(AUTHORIZATION_TOKEN)
-        return config
-    }, err => {
-        console.log(err)
-        loadingInstance && loadingInstance.close()
-        return ElMessage.error({message: '请求超时，请检查网络设置!'})
-    }
+  (config) => {
+    const {data, showLoading} = config.data as configDataType
+    if (showLoading) loadingInstance = createLoadingInstance()
+    config.data = data
+    const token = localStorageMethods.getLocalStorage(AUTHORIZATION_TOKEN)
+    if (token) config.headers['Authorization'] = 'Bearer ' + localStorageMethods.getLocalStorage(AUTHORIZATION_TOKEN)
+    return config
+  }, err => {
+    console.log(err)
+    loadingInstance && loadingInstance.close()
+    return ElMessage.error({message: '请求超时，请检查网络设置!'})
+  }
 )
 
 axiosInstance.interceptors.response.use(
-    (resp) => {
-        loadingInstance && loadingInstance.close()
-        _handleTimeOut(resp.data)
-        return Promise.resolve(resp)
-    }, err => {
-        loadingInstance && loadingInstance.close()
-        _handleTimeOut(err.response.data)
+  (resp) => {
+    loadingInstance && loadingInstance.close()
+    _handleTimeOut(resp.data)
+    return Promise.resolve(resp)
+  }, err => {
+    loadingInstance && loadingInstance.close()
+    _handleTimeOut(err.response.data)
 
-        if (err.response.status === 504 || err.response.status === 404)
-            ElMessage.error({message: '服务器被吃了⊙﹏⊙∥'})
-        else if (err.response.status === 403)
-            ElMessage.error({message: '权限不足,请联系管理员!'})
-        else if (err.response.status === 500)
-            ElMessage.error({message: '系统错误,联系管理员!'})
-        else
-            console.log(err)
-        throw new Error(err)
-    }
+    if (err.response.status === 504 || err.response.status === 404)
+      ElMessage.error({message: '服务器被吃了⊙﹏⊙∥'})
+    else if (err.response.status === 403)
+      ElMessage.error({message: '权限不足,请联系管理员!'})
+    else if (err.response.status === 500)
+      ElMessage.error({message: '系统错误,联系管理员!'})
+    else
+      console.log(err)
+    throw new Error(err)
+  }
 )
 
 const _handleTimeOut = function (data: ResponseDataType) {
-    if (data.status != null) {
-        if (data.status.code === errCode.SESSION_TIME_OUT) {
-            // localStorageMethods.removeLocalStorage(AUTHORIZATION_TOKEN)
-            // localStorageMethods.removeLocalStorage('operator')
-            let redirectUri
-            if (isNotEmpty(import.meta.env.VITE_ssoDomain)) {
-                redirectUri = import.meta.env.VITE_ssoDomain
-                if (redirectUri === 'localhost') {
-                    const url = removeURLParameter(window.location.href, 'redirect_uri').split('#/')[1]
-                    const redirect_uri = url === 'login' ? undefined : url
-                    return router.replace({
-                        path: ssoLoginUrl,
-                        query: {redirect_uri} as LocationQueryRaw
-                    }).then(() => window.location.reload())
-                }
-            } else {
-                const url = window.location.href.split('://')[1]
-                const http = window.location.href.split(url)[0]
-                redirectUri = http + url.split('?')[0].split('/')[0] + '/sso'
-            }
-            const targetUrl = encodeURIComponent(window.location.href)
-            window.location.href = ssoLoginUrl + '&redirect_uri=' + redirectUri + '&target_url=' + targetUrl
-        }
+  if (data.status != null) {
+    if (data.status.code === errCode.SESSION_TIME_OUT) {
+      navigation2Login()?.then()
     }
+  }
 }
 
 function get(apiType: ApiType,
@@ -101,7 +80,7 @@ function get(apiType: ApiType,
              showSuccess = false,
              showLoading = true,
              showErr = true) {
-    return request(apiType, params, data, showSuccess, showLoading, showErr)
+  return request(apiType, params, data, showSuccess, showLoading, showErr)
 }
 
 function post(apiType: ApiType,
@@ -110,7 +89,7 @@ function post(apiType: ApiType,
               showSuccess = true,
               showLoading = true,
               showErr = true) {
-    return request(apiType, params, data, showSuccess, showLoading, showErr)
+  return request(apiType, params, data, showSuccess, showLoading, showErr)
 }
 
 // apiType method/url/version
@@ -125,107 +104,107 @@ function request(apiType: ApiType,
                  showSuccess = false,
                  showLoading = true,
                  showErr = true) {
-    return axiosInstance({
-        baseURL: import.meta.env.VITE_baseURL + apiType.baseDomain + web,
-        method: apiType.method,
-        url: apiType.url + '?' + qs.stringify(params, {arrayFormat: 'repeat'}),
-        data: {
-            data: body,
-            showLoading
-        },
-        params: null,
-        headers: {
-            'Content-Type': 'application/json',
-            'api-version': apiType.version,
-            'client-type': 0
+  return axiosInstance({
+    baseURL: import.meta.env.VITE_baseURL + apiType.baseDomain + web,
+    method: apiType.method,
+    url: apiType.url + '?' + qs.stringify(params, {arrayFormat: 'repeat'}),
+    data: {
+      data: body,
+      showLoading
+    },
+    params: null,
+    headers: {
+      'Content-Type': 'application/json',
+      'api-version': apiType.version,
+      'client-type': 0
+    }
+  }).then(resp => {
+    if (resp.data.status?.code !== errCode.SUCCESS) {
+      if (resp.data.status?.msg) {
+        const errTypeMapList = ['info', 'warning', 'error']
+        const errLevel = +resp.data.payload.errLevel
+        const errType = errTypeMapList[errLevel]
+        if (showErr) {
+          ElMessage.error({message: resp.data.payload.errMsg, type: errType})
+          throw new Error(resp.data.status.msg)
+        } else {
+          throw new Error(resp.data.status.details)
         }
-    }).then(resp => {
-        if (resp.data.status?.code !== errCode.SUCCESS) {
-            if (resp.data.status?.msg) {
-                const errTypeMapList = ['info', 'warning', 'error']
-                const errLevel = +resp.data.payload.errLevel
-                const errType = errTypeMapList[errLevel]
-                if (showErr) {
-                    ElMessage.error({message: resp.data.payload.errMsg, type: errType})
-                    throw new Error(resp.data.status.msg)
-                } else {
-                    throw new Error(resp.data.status.details)
-                }
-            }
-        } else if (showSuccess) {
-            const message = resp.data.status.details
-            if (message) ElMessage.success({message})
-        }
-        return {
-            status: resp.data.status,
-            payload: resp.data.payload,
-            response: resp
-        }
-    }, err => {
-        loadingInstance && loadingInstance.close()
-        ElMessage.error({message: '后台接口错误，请联系后台管理员！'})
-        throw new Error(err)
-    })
+      }
+    } else if (showSuccess) {
+      const message = resp.data.status.details
+      if (message) ElMessage.success({message})
+    }
+    return {
+      status: resp.data.status,
+      payload: resp.data.payload,
+      response: resp
+    }
+  }, err => {
+    loadingInstance && loadingInstance.close()
+    ElMessage.error({message: '后台接口错误，请联系后台管理员！'})
+    throw new Error(err)
+  })
 }
 
 function download(
-    apiType: ApiType,
-    fileName: string,
-    params: object,
-    body: object) {
-    ElMessage.success({message: '开始下载……'})
-    return axiosInstance({
-        baseURL: import.meta.env.VITE_baseURL + apiType.baseDomain + web,
-        method: apiType.method,
-        url: apiType.url + '?' + qs.stringify(params, {arrayFormat: 'repeat'}),
-        data: {data: body},
-        params: null,
-        headers: {
-            'Content-Type': 'application/json',
-            'api-version': apiType.version,
-            'client-type': 0
-        },
-        responseType: 'blob'
-    }).then(resp => {
-        _download(resp, fileName)
-        return {
-            status: resp.data.status,
-            payload: resp.data.payload,
-            response: resp
-        }
-    }, err => {
-        ElMessage.error({message: '下载失败，请检查网络后重试，或联系系统管理员！'})
-        console.log(err)
-        throw new Error(err)
-    })
+  apiType: ApiType,
+  fileName: string,
+  params: object,
+  body: object) {
+  ElMessage.success({message: '开始下载……'})
+  return axiosInstance({
+    baseURL: import.meta.env.VITE_baseURL + apiType.baseDomain + web,
+    method: apiType.method,
+    url: apiType.url + '?' + qs.stringify(params, {arrayFormat: 'repeat'}),
+    data: {data: body},
+    params: null,
+    headers: {
+      'Content-Type': 'application/json',
+      'api-version': apiType.version,
+      'client-type': 0
+    },
+    responseType: 'blob'
+  }).then(resp => {
+    _download(resp, fileName)
+    return {
+      status: resp.data.status,
+      payload: resp.data.payload,
+      response: resp
+    }
+  }, err => {
+    ElMessage.error({message: '下载失败，请检查网络后重试，或联系系统管理员！'})
+    console.log(err)
+    throw new Error(err)
+  })
 }
 
 function upload(
-    apiType: ApiType,
-    params: object,
-    body: object,
-    onUploadProgress: Function) {
-    return axiosInstance({
-        baseURL: import.meta.env.VITE_baseURL + apiType.baseDomain + web,
-        method: apiType.method,
-        url: apiType.url + '?' + qs.stringify(params, {arrayFormat: 'repeat'}),
-        data: {data: body},
-        params: null,
-        headers: {'Content-Type': 'multipart/form-data'},
-        onUploadProgress: (progressEvent: AxiosProgressEvent) => {
-            onUploadProgress(progressEvent)
-        }
-    }).then(resp => {
-        return {
-            status: resp.data.status,
-            payload: resp.data.payload,
-            response: resp
-        }
-    }).catch(err => {
-        ElMessage.error({message: '上传失败，请检查网络后重试，或联系系统管理员！'})
-        console.log(err)
-        throw new Error(err)
-    })
+  apiType: ApiType,
+  params: object,
+  body: object,
+  onUploadProgress: Function) {
+  return axiosInstance({
+    baseURL: import.meta.env.VITE_baseURL + apiType.baseDomain + web,
+    method: apiType.method,
+    url: apiType.url + '?' + qs.stringify(params, {arrayFormat: 'repeat'}),
+    data: {data: body},
+    params: null,
+    headers: {'Content-Type': 'multipart/form-data'},
+    onUploadProgress: (progressEvent: AxiosProgressEvent) => {
+      onUploadProgress(progressEvent)
+    }
+  }).then(resp => {
+    return {
+      status: resp.data.status,
+      payload: resp.data.payload,
+      response: resp
+    }
+  }).catch(err => {
+    ElMessage.error({message: '上传失败，请检查网络后重试，或联系系统管理员！'})
+    console.log(err)
+    throw new Error(err)
+  })
 }
 
 
@@ -233,26 +212,26 @@ function upload(
 
 function _download(res: AxiosResponse, fileName: string) {
 
-    const content = res.data
-    // 构造一个blob对象来处理数据
-    const blob = new Blob([content])
+  const content = res.data
+  // 构造一个blob对象来处理数据
+  const blob = new Blob([content])
 
-    // 对于<a>标签，只有 Firefox 和 Chrome（内核） 支持 download 属性
-    // IE10以上支持blob但是依然不支持download
-    if ('download' in document.createElement('a')) { // 支持a标签download的浏览器
-        const link = document.createElement('a') // 创建a标签
-        link.download = fileName // a 标签添加属性
-        link.style.display = 'none'
-        link.href = URL.createObjectURL(blob)
-        document.body.appendChild(link)
-        link.click() // 执行下载
-        URL.revokeObjectURL(link.href) // 释放url
-        document.body.removeChild(link) // 释放标签
-    } else { // 其他浏览器
-        navigator.msSaveBlob && navigator.msSaveBlob(blob, fileName)
-    }
+  // 对于<a>标签，只有 Firefox 和 Chrome（内核） 支持 download 属性
+  // IE10以上支持blob但是依然不支持download
+  if ('download' in document.createElement('a')) { // 支持a标签download的浏览器
+    const link = document.createElement('a') // 创建a标签
+    link.download = fileName // a 标签添加属性
+    link.style.display = 'none'
+    link.href = URL.createObjectURL(blob)
+    document.body.appendChild(link)
+    link.click() // 执行下载
+    URL.revokeObjectURL(link.href) // 释放url
+    document.body.removeChild(link) // 释放标签
+  } else { // 其他浏览器
+    navigator.msSaveBlob && navigator.msSaveBlob(blob, fileName)
+  }
 }
 
 
-export {baseURL, get, post, request, upload, download}
+export { baseURL, get, post, request, upload, download }
 
