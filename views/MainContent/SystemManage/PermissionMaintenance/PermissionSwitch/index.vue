b164bd0ec56c37958cb1684040be4a2526b18e38
zhifusong <1253903640@qq.com>
2024-06-26 09:14:06 +0800

    格式化了权限切换组件

--- views/MainContent/SystemManage/PermissionMaintenance/PermissionSwitch/index.vue
+++ views/MainContent/SystemManage/PermissionMaintenance/PermissionSwitch/index.vue
@@ -2,19 +2,19 @@
   <div class="wrapper">
     <a-typography-title :level="2">将系统超级管理员的权限切换到指定用户</a-typography-title>
     <br />
-    <department-and-staff-select layout-mode="vertical" v-model:staffListValue="staffListValue" :is-multiple="false" />
-    <a-button type="primary" block @click="switchPermission">切换</a-button>
+    <department-and-staff-select v-model:staffListValue="staffListValue" :is-multiple="false" layout-mode="vertical"/>
+    <a-button block type="primary" @click="switchPermission">切换</a-button>
   </div>
 </template>
 
-<script setup lang="ts">
-import {Ref} from "vue"
-import {message} from "ant-design-vue"
-import {ValueLabelArray} from "@/framework/utils/type"
-import {ghostLogin} from "@/framework/apis/login/login";
-import {localStorageMethods} from "@/framework/utils/common";
-import {AUTHORIZATION_TOKEN, REFRESH_TOKEN} from "@/framework/utils/constant";
-import {afterLogin} from "@/framework/network/login";
+<script lang="ts" setup>
+import {Ref} from 'vue'
+import {message} from 'ant-design-vue'
+import {ValueLabelArray} from '@/framework/utils/type'
+import {ghostLogin} from '@/framework/apis/login/login'
+import {localStorageMethods} from '@/framework/utils/common'
+import {AUTHORIZATION_TOKEN, REFRESH_TOKEN} from '@/framework/utils/constant'
+import {afterLogin} from '@/framework/network/login'
 
 const router = useRouter()
 const staffListValue: Ref<ValueLabelArray> = ref([])
@@ -23,16 +23,13 @@ const switchPermission = () => {
     message.error({content: () => '请选择用户后再执行权限切换操作', style: {marginTop: '10vh', marginLeft: '150px'}})
     return
   }
-  const { customerNumber } = staffListValue.value[0].option
+  const {customerNumber} = staffListValue.value[0].option
   ghostLogin(customerNumber).then(res => {
     localStorageMethods.setLocalStorage(REFRESH_TOKEN, res.payload[REFRESH_TOKEN])
     localStorageMethods.setLocalStorage(AUTHORIZATION_TOKEN, res.payload.accessToken)
     afterLogin().then(() => router.replace('/')).then(() => window.location.reload())
   })
 }
-
-
-
 
 </script>
 
