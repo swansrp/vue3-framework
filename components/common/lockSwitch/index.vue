f0c1e00b9caf4340ea69d9bd7ed2581f05a960c2
swansrp <56093273@qq.com>
2024-08-15 13:27:30 +0800

    添加锁头switch组件

---
+++ components/common/lockSwitch/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <lock-outlined
+    v-if="modelValue"
+    @click="() => {
+      emit('update:modelValue', !modelValue)
+      emit('onClick')
+    }" />
+  <unlock-outlined
+    v-else
+    @click="() => {
+      emit('update:modelValue', !modelValue)
+      emit('onClick')
+    }" />
+</template>
+
+<script lang="ts" setup>
+import { LockOutlined, UnlockOutlined } from '@ant-design/icons-vue'
+
+const props = withDefaults(
+  defineProps<{
+    modelValue: boolean
+  }>(),
+  {
+    modelValue: true
+  }
+)
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: any): void
+  (e: 'onClick'): void
+}>()
+const {modelValue} = toRefs(props)
+onMounted(() => {
+})
+</script>
+
+<style lang="less" scoped></style>
