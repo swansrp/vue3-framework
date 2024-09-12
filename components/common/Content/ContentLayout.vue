053ec46fe21edd30179a854e4b0ddd947fe623e1
swansrp <56093273@qq.com>
2024-09-12 09:21:36 +0800

    目录布局侧边栏可以拖拽调整大小

--- components/common/Content/ContentLayout.vue
+++ components/common/Content/ContentLayout.vue
@@ -1,13 +1,15 @@
 <template>
   <a-layout>
     <a-layout-sider
-      :width="width"
+      id="side"
+      :width="_width"
       class="side-wrapper"
     >
       <div style="margin: 10px">
         <slot name="side"></slot>
       </div>
     </a-layout-sider>
+    <div id="resize"></div>
     <a-layout-content
       style="margin-left: 10px; margin-right: 10px">
       <slot name="content"></slot>
@@ -24,8 +26,42 @@ const props = withDefaults(
     width: '15%'
   }
 )
-const { width } = toRefs(props)
+const _width = ref(props.width)
+const widthValue = ref(0)
+const dragControllerDiv = () => {
+  const resize = document.getElementById("resize")
+  const side = document.getElementById("side")
+  widthValue.value = side?.offsetLeft || 0
+  if (resize) {
+    let startX = resize.offsetLeft
+    resize.onmousedown = (e) => {
+      // 颜色改变提醒
+      resize.style.background = "#818181"
+      document.onmousemove = (e) => {
+        // 计算并应用位移量
+        let endX = e.clientX
+        let moveLen = endX - startX
+        startX = endX
+        widthValue.value += moveLen
+        _width.value = widthValue.value + 'px'
+      };
+      document.onmouseup = () => {
+        // 颜色恢复
+        resize.style.background = ""
+        document.onmousemove = null
+        document.onmouseup = null
+      }
+      return false
+    }
+  }
+}
+watch(
+  () => _width.value,
+  () => console.log(_width.value)
+)
+
 onMounted(() => {
+  dragControllerDiv()
 })
 </script>
 
@@ -37,5 +73,12 @@ onMounted(() => {
   height: calc(100vh - 150px);
   box-shadow: 0 4px 10px 0 rgba(69, 89, 120, 0.5);
   margin: 10px 15px;
+}
+
+#resize {
+  background: rgba(69, 89, 120, 0.05);
+  width: 5px;
+  height: 100vh;
+  cursor: w-resize;
 }
 </style>
