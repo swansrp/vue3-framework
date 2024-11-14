94409df66157f1158dab9f8812ba4df6180df014
swansrp <56093273@qq.com>
2024-11-14 14:57:50 +0800

    添加员工显示store

---
+++ store/staff.ts
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+import { getStaff } from "@/framework/components/common/departmentAndStaffSelect/api";
+
+export const useStaffStore = defineStore('staffStore', {
+  state: () => {
+    return {
+      staffObj: {} as any
+    }
+  },
+  actions: {
+    async getStaffInfo(customerNumber: string) {
+      const staffInfo = this.staffObj[customerNumber]
+      if (staffInfo) return staffInfo
+      return await getStaff([customerNumber]).then(({payload}) => {
+        this.staffObj[customerNumber] = payload[0]
+        return payload[0]
+      })
+    }
+  }
+})
