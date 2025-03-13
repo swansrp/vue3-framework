afaccea7e659ba11894ceed13ff74f60de9e71e7
swansrp <56093273@qq.com>
2025-03-13 16:17:07 +0800

    添加rollup配置

--- config/vite.config.ts
+++ config/vite.config.ts
@@ -13,6 +13,17 @@ import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  },
   plugins: [
     vue(),
     vueJsx(),
