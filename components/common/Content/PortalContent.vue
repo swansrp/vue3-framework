17e33db2c965f90ffb345c0a6b3056e5f30336a5
swansrp <56093273@qq.com>
2025-04-09 15:10:43 +0800

    优化portal作为筛选栏相关配置和逻辑

--- components/common/Content/PortalContent.vue
+++ components/common/Content/PortalContent.vue
@@ -1,6 +1,7 @@
 <template>
   <portal
     v-if="treeMode"
+    ref="portalRef"
     :check-strictly="checkStrictly"
     :table-id="tableId"
     :tree-check-able="multi"
@@ -9,8 +10,13 @@
     tree-mode
     @selected-data="onSelectedData" />
   <portal
-    v-else :table-id="tableId"
+    v-else
+    ref="portalRef"
+    :hide-row-selection="!multi"
+    :row-select-props="rowSelectProps"
+    :table-id="tableId"
     check-strictly="check-strictly"
+    hide-association
     list-mode
     mode-lock
     read-only
@@ -21,6 +27,7 @@
 import { Ref } from 'vue'
 import { isNotEmpty } from '@/framework/utils/common'
 
+const portalRef = ref()
 const props = withDefaults(
   defineProps<{
     tableId: string
@@ -28,21 +35,27 @@ const props = withDefaults(
     treeMode?: boolean
     checkStrictly?: boolean
     multi?: boolean
+    valueField?: string
     labelField?: string
     disable?: boolean
     searchAble?: boolean
+    rowSelectProps?: Function
+    labelInValue?: boolean
   }>(),
   {
     modelValue: undefined,
     treeMode: false,
     checkStrictly: false,
     multi: false,
-    labelField: 'label',
+    valueField: 'id',
+    labelField: 'name',
     disable: false,
-    searchAble: false
+    searchAble: false,
+    rowSelectProps: undefined,
+    labelInValue: false
   }
 )
-const {treeMode, modelValue, tableId, multi} = toRefs(props)
+const { treeMode, modelValue, tableId, multi, valueField, labelField, labelInValue } = toRefs(props)
 const emit = defineEmits<{
   (e: 'update:modelValue', value: any): void
   (e: 'change', value: any, label: any): void
@@ -63,21 +76,41 @@ const onSelectedData = (data: any) => {
   if (treeMode.value) {
     selectedData.value = data || []
   } else {
-    if(multi.value) {
-      selectedData.value = [data]?.map((item: any) => {
-        return item.value
-      })
+    if (multi.value) {
+      if (isNotEmpty(data)) {
+        try {
+          selectedData.value = [...data]?.map((item: any) => {
+            if (labelInValue.value) {
+              return { value: item[valueField.value], label: item[labelField.value], data: item }
+            } else {
+              return item[valueField.value]
+            }
+          })
+        } catch (e) {
+
+        }
+      } else {
+        selectedData.value = []
+      }
+
     } else {
-      if(isNotEmpty(data)) {
-        selectedData.value = [data.value]
+      if (isNotEmpty(data)) {
+        if (labelInValue.value) {
+          selectedData.value = [{ value: data[valueField.value], label: data[labelField.value] }]
+        } else {
+          selectedData.value = [data[valueField.value]]
+        }
       } else {
         selectedData.value = []
       }
     }
   }
   console.log('onSelectedData', selectedData.value)
-
 }
+const forceUpdate = () => {
+  portalRef.value.queryData()
+}
+defineExpose({ forceUpdate })
 onMounted(() => {
 })
 </script>
