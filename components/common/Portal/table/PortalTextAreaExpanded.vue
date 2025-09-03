2b5bd234832c08274004041cd6b6ac2b45fa272f
swansrp <56093273@qq.com>
2025-09-03 14:17:36 +0800

    添加3方对接记录

--- components/common/Portal/table/PortalTextAreaExpanded.vue
+++ components/common/Portal/table/PortalTextAreaExpanded.vue
@@ -2,14 +2,14 @@
   <a-tabs v-model:activeKey="activeKey">
     <a-tab-pane
       v-for="(item, index) of columns" :key="index" :disabled="isEmpty(record[item.dataIndex])"
-      :tab="strLF2HtmlLF(item.title)">
+      :tab="strRemoveLF(item.title)">
       <div v-html="strLF2HtmlLF(record[item.dataIndex])"></div>
     </a-tab-pane>
   </a-tabs>
 </template>
 
 <script lang="ts" setup>
-import { isEmpty, isNotEmpty, strLF2HtmlLF } from '@/framework/utils/common'
+import { isEmpty, isNotEmpty, strLF2HtmlLF, strRemoveLF } from '@/framework/utils/common'
 
 const props = withDefaults(
   defineProps<{
@@ -18,7 +18,7 @@ const props = withDefaults(
   }>(),
   {}
 )
-const {record, columns} = toRefs(props)
+const { record, columns } = toRefs(props)
 const activeKey = ref(isNotEmpty(record.value[columns.value[0].dataIndex]) ? 0 : -1)
 
 onMounted(() => {
---
+++ views/MainContent/Portal/channelHistory/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <portal table-id="AcPartnerHistory" read-only />
+</template>
+
+<script lang="ts" setup>
+
+onMounted(() => {
+})
+</script>
+
+<style scoped lang="less"></style>
