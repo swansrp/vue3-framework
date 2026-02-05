b7d532233a69b13f0a2f146c5a9f794e64d4f646
swansrp <56093273@qq.com>
2026-02-05 16:03:47 +0800

    添加人员配置角色相关接口

--- apis/admin/userRole.ts
+++ apis/admin/userRole.ts
@@ -1,8 +1,9 @@
 import { apiType, buildGetApiByType, buildPostApiByType } from '@/framework/apis'
 import { request } from '@/framework/network/request'
 
-const _buildPostApi = (url: string) => buildPostApiByType(url, apiType.userRole)
+const buildPostApi = (url: string) => buildPostApiByType(url, apiType.userRole)
 const buildGetApi = (url: string) => buildGetApiByType(url, apiType.userRole)
 
 export const getUserRoleList = (entityId: string) => request(buildGetApi('/bind/list'),{ entityId }, {}, false, false) as Promise<any>
 export const getUserPermissionTree = (userId: string) => request(buildGetApi('/menu/tree'),{ userId }, {}, false, false) as Promise<any>
+export const replaceUserRoleList = (entityId: string, attachIdList: Array<string|number>) => request(buildPostApi('/replace'),{}, { entityId, attachIdList }, true, false) as Promise<any>
